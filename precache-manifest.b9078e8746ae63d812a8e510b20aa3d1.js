2ecf911031b22175dc5c1a1175287408b9861c76
Saulet Yskak <sauletik01@gmail.com>
2019-05-22 20:02:34 +0600

    Updates

---
+++ precache-manifest.b9078e8746ae63d812a8e510b20aa3d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "970bdd102fe64672a7fd9e2cf1c5fb0a",
+    "url": "/Weather/index.html"
+  },
+  {
+    "revision": "b4d933f7dd497f1f1641",
+    "url": "/Weather/static/css/main.9fa929d7.chunk.css"
+  },
+  {
+    "revision": "5ea18b38052a32e876fc",
+    "url": "/Weather/static/js/2.0970fd17.chunk.js"
+  },
+  {
+    "revision": "b4d933f7dd497f1f1641",
+    "url": "/Weather/static/js/main.b7ba019c.chunk.js"
+  },
+  {
+    "revision": "e6172640d3267d8b322d",
+    "url": "/Weather/static/js/runtime~main.72be58e2.js"
+  },
+  {
+    "revision": "2590c4f70fb628e93ebdfef30abe49b5",
+    "url": "/Weather/static/media/placeholder.2590c4f7.svg"
+  },
+  {
+    "revision": "58600a308ba0b2880f1fb01b2be8e973",
+    "url": "/Weather/static/media/umbrella.58600a30.svg"
+  }
+]);
